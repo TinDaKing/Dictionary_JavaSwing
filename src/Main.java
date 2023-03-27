933d8fbf22675a4275c34dd840b31cc9714fa708
TinDaKing <tranhoangtin3702@gmail.com>
2023-03-27 18:19:41 +0700

    Delete word ok, fix bug Layout

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,8 @@
-import dao.EngViet;
-import dao.FavoriteList;
-import dao.VietEng;
 import view.ScreenView;
 
 public class Main {
     public static void main(String[] args) {
-
-        ScreenView sv = new ScreenView();
+        new ScreenView();
 
     }
 }
--- src/view/ScreenView.java
+++ src/view/ScreenView.java
@@ -7,10 +7,9 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Comparator;
 import java.util.List;
 
-public class ScreenView extends JFrame{
+public class ScreenView extends JFrame {
 
     JPanel topBar;
     JTextField searchField;
@@ -22,10 +21,11 @@ public class ScreenView extends JFrame{
     JButton addNewWord;
     JButton removeWord;
     JComboBox dictType;
-    JTextField newWordText;
+    JTextField wordText;
     JComboBox wordType;
     JTextArea meaningText;
     JButton submitAddWord;
+    JButton submitRemoveWord;
 
     public ScreenView() {
 
@@ -38,28 +38,27 @@ public class ScreenView extends JFrame{
 
         topBar = new JPanel();
         topBar.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 30));
-        topBar.setBackground(new Color(20,108,148,150));
+        topBar.setBackground(new Color(20, 108, 148, 150));
         initTopBar();
 
         leftBar = new JPanel();
         leftBar.setLayout(new BoxLayout(leftBar, BoxLayout.Y_AXIS));
-        leftBar.setBackground(new Color(20,108,148,80));
-        leftBar.setPreferredSize(new Dimension(150,100));
+        leftBar.setBackground(new Color(20, 108, 148, 80));
+        leftBar.setPreferredSize(new Dimension(150, 100));
         initLeftBar();
 
         rightBar = new JPanel();
         rightBar.setLayout(new BoxLayout(rightBar, BoxLayout.Y_AXIS));
 
 
-
         add(topBar, BorderLayout.NORTH);
-        add(leftBar,BorderLayout.WEST);
-        add(rightBar,BorderLayout.EAST);
+        add(leftBar, BorderLayout.WEST);
+        add(rightBar, BorderLayout.EAST);
         setVisible(true);
 
     }
 
-    private void initTopBar(){
+    private void initTopBar() {
         searchField = new JTextField();
         // searchField.setText("Enter a word");
         searchField.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
@@ -80,16 +79,18 @@ public class ScreenView extends JFrame{
 
         addListenerSearchButton();
     }
+
     private void addListenerSearchButton() {
         searchButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 try {
                     if (midPanel == null) {
                         midPanel = new JPanel();
-                        midPanel.setLayout(new BoxLayout(midPanel, BoxLayout.Y_AXIS));
                     } else {
                         midPanel.removeAll();
                     }
+                    midPanel.setLayout(new BoxLayout(midPanel, BoxLayout.Y_AXIS));
+
                     JLabel meaning = null;
 
                     int lng = languageList.getSelectedIndex();
@@ -98,7 +99,7 @@ public class ScreenView extends JFrame{
                             JOptionPane.showMessageDialog(null, "Cannot find your word! Please try again ^^");
                         } else {
                             List<String> labels = EngViet.getInstance().translateWord(searchField.getText());
-                            for (String l: labels){
+                            for (String l : labels) {
                                 meaning = new JLabel(l);
                                 meaning.setAlignmentX(Component.LEFT_ALIGNMENT);
                                 midPanel.add(meaning);
@@ -110,7 +111,7 @@ public class ScreenView extends JFrame{
                             JOptionPane.showMessageDialog(null, "Cannot find your word! Please try again ^^");
                         } else {
                             List<String> labels = VietEng.getInstance().translateWord(searchField.getText());
-                            for (String l: labels){
+                            for (String l : labels) {
                                 meaning = new JLabel(l);
                                 meaning.setAlignmentX(Component.LEFT_ALIGNMENT);
                                 midPanel.add(meaning);
@@ -132,7 +133,7 @@ public class ScreenView extends JFrame{
         });
     }
 
-    private void initLeftBar(){
+    private void initLeftBar() {
         addNewWord = new JButton("Add New Word");
         addNewWord.setAlignmentX(Component.CENTER_ALIGNMENT);
 
@@ -145,6 +146,7 @@ public class ScreenView extends JFrame{
         leftBar.add(removeWord);
 
         addListenerAddNewWordButton();
+        addListenerRemoveWordButton();
     }
 
     private void addListenerAddNewWordButton() {
@@ -153,14 +155,15 @@ public class ScreenView extends JFrame{
                 try {
                     if (midPanel == null) {
                         midPanel = new JPanel();
-                        midPanel.setLayout(new BoxLayout(midPanel, BoxLayout.Y_AXIS));
                     } else {
                         midPanel.removeAll();
                     }
-                    String[] dicts ={"EngVie","VieEng"};
+                    midPanel.setLayout(new BoxLayout(midPanel, BoxLayout.Y_AXIS));
+
+                    String[] dicts = {"EngVie", "VieEng"};
                     JLabel dictLabel = new JLabel("Dictionary: ");
-                    dictLabel.setBounds(0,0,40,20);
-                    if (dictType!=null)
+                    dictLabel.setBounds(0, 0, 40, 20);
+                    if (dictType != null)
                         dictType.removeAll();
                     dictType = new JComboBox(dicts);
                     dictType.setSelectedIndex(0);
@@ -171,22 +174,22 @@ public class ScreenView extends JFrame{
                     panel0.setAlignmentX(Component.LEFT_ALIGNMENT);
 
                     JLabel newWordLabel = new JLabel("New word: ");
-                    newWordLabel.setBounds(0,0,40,30);
-                    if (newWordText!=null)
-                        newWordText.removeAll();
-                    newWordText = new JTextField();
-                    newWordText.setPreferredSize(new Dimension(100,20));
+                    newWordLabel.setBounds(0, 0, 40, 30);
+                    if (wordText != null)
+                        wordText.removeAll();
+                    wordText = new JTextField();
+                    wordText.setPreferredSize(new Dimension(100, 20));
 
                     JPanel panel1 = new JPanel(new FlowLayout(10));
                     panel1.add(newWordLabel);
-                    panel1.add(newWordText);
+                    panel1.add(wordText);
                     panel1.setAlignmentX(Component.LEFT_ALIGNMENT);
 
-                    String[] languages = {"Noun/Danh từ", "Verb/Động từ", "Adjective/Tính từ", "Adverb/Trạng từ", "Pronoun/Đại từ","Preposition/Giới từ"};
+                    String[] languages = {"Noun/Danh từ", "Verb/Động từ", "Adjective/Tính từ", "Adverb/Trạng từ", "Pronoun/Đại từ", "Preposition/Giới từ"};
 
                     JLabel typeLabel = new JLabel("Type: ");
-                    typeLabel.setBounds(0,0,40,20);
-                    if (wordType!=null)
+                    typeLabel.setBounds(0, 0, 40, 20);
+                    if (wordType != null)
                         wordType.removeAll();
                     wordType = new JComboBox(languages);
                     wordType.setSelectedIndex(0);
@@ -197,11 +200,11 @@ public class ScreenView extends JFrame{
                     panel2.setAlignmentX(Component.LEFT_ALIGNMENT);
 
                     JLabel meaningLabel = new JLabel("Meaning: ");
-                    meaningLabel.setBounds(0,0,40,20);
-                    if (meaningText!=null)
+                    meaningLabel.setBounds(0, 0, 40, 20);
+                    if (meaningText != null)
                         meaningText.removeAll();
                     meaningText = new JTextArea();
-                    meaningText.setPreferredSize(new Dimension(300,150));
+                    meaningText.setPreferredSize(new Dimension(300, 150));
                     meaningText.setWrapStyleWord(true);
                     meaningText.setLineWrap(true);
 
@@ -210,7 +213,7 @@ public class ScreenView extends JFrame{
                     panel3.add(meaningText);
                     panel3.setAlignmentX(Component.LEFT_ALIGNMENT);
 
-                    if(submitAddWord !=null){
+                    if (submitAddWord != null) {
                         submitAddWord.removeAll();
                     }
                     submitAddWord = new JButton("Add Word");
@@ -240,25 +243,124 @@ public class ScreenView extends JFrame{
         submitAddWord.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 try {
-                    if (newWordText == null || meaningText == null) {
+                    if (wordText == null || meaningText == null) {
                         JOptionPane.showMessageDialog(null, "Please fill the text field ^^");
                         return;
                     }
                     if (dictType.getSelectedIndex() == 0) { //ENG_VIE
-                        EngViet.getInstance().addNewWord(newWordText.getText(), wordType.getSelectedItem() + "\n- " + meaningText.getText());
-                    } else{
-                        VietEng.getInstance().addNewWord(newWordText.getText(), wordType.getSelectedItem() + "\n- " + meaningText.getText());
+                        if (!EngViet.getInstance().addNewWord(wordText.getText(), wordType.getSelectedItem() + "\n- " + meaningText.getText())) {
+                            JOptionPane.showMessageDialog(null, "This word already existed ^^ You can remove it first");
+                            return;
+                        }
+                    } else {
+                        if (!VietEng.getInstance().addNewWord(wordText.getText(), wordType.getSelectedItem() + "\n- " + meaningText.getText())) {
+                            JOptionPane.showMessageDialog(null, "This word already existed ^^ You can remove it first");
+                            return;
+                        }
                     }
 
                     midPanel.removeAll();
                     repaint();
                     setVisible(true);
+                    JOptionPane.showMessageDialog(null, "Add successfully ^^");
 
-                }catch (Exception ex) {
+                } catch (Exception ex) {
                     ex.printStackTrace();
                 }
             }
         });
     }
 
+    private void addListenerRemoveWordButton() {
+        removeWord.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    if (midPanel == null) {
+                        midPanel = new JPanel();
+                    } else {
+                        midPanel.removeAll();
+                    }
+                    midPanel.setLayout(new FlowLayout());
+
+
+                    String[] dicts = {"EngVie", "VieEng"};
+                    JLabel dictLabel = new JLabel("Dictionary: ");
+                    dictLabel.setBounds(0, 0, 40, 20);
+                    if (dictType != null)
+                        dictType.removeAll();
+                    dictType = new JComboBox(dicts);
+                    dictType.setSelectedIndex(0);
+
+                    JPanel panel0 = new JPanel(new FlowLayout(10));
+                    panel0.add(dictLabel);
+                    panel0.add(dictType);
+                    panel0.setAlignmentX(Component.LEFT_ALIGNMENT);
+
+                    JLabel newWordLabel = new JLabel("Word to delete: ");
+                    newWordLabel.setBounds(0, 0, 40, 30);
+                    if (wordText != null)
+                        wordText.removeAll();
+                    wordText = new JTextField();
+                    wordText.setPreferredSize(new Dimension(100, 20));
+
+                    JPanel panel1 = new JPanel(new FlowLayout(10));
+                    panel1.add(newWordLabel);
+                    panel1.add(wordText);
+                    panel1.setAlignmentX(Component.LEFT_ALIGNMENT);
+
+                    if (submitRemoveWord != null) {
+                        submitRemoveWord.removeAll();
+                    }
+                    submitRemoveWord = new JButton("Delete Word");
+                    submitRemoveWord.setAlignmentX(Component.LEFT_ALIGNMENT);
+                    addListenerSubmitRemoveWord();
+
+                    midPanel.add(panel0);
+                    midPanel.add(panel1);
+                    midPanel.add(submitRemoveWord);
+                    midPanel.add(new JLabel(" "));
+                    midPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+
+                    add(midPanel, BorderLayout.CENTER);
+                    repaint();
+                    setVisible(true);
+
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void addListenerSubmitRemoveWord() {
+        submitRemoveWord.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    if (wordText == null) {
+                        JOptionPane.showMessageDialog(null, "Please fill the text field ^^");
+                        return;
+                    }
+                    if (dictType.getSelectedIndex() == 0) { //ENG_VIE
+                        if (!EngViet.getInstance().removeWord(wordText.getText())) {
+                            JOptionPane.showMessageDialog(null, "This word not existed, cannot be deleted ^^");
+                            return;
+                        }
+                    } else { //VIE_ENG
+                        if (!VietEng.getInstance().removeWord(wordText.getText())) {
+                            JOptionPane.showMessageDialog(null, "This word not existed, cannot be deleted ^^");
+                            return;
+                        }
+                    }
+
+                    midPanel.removeAll();
+                    repaint();
+                    setVisible(true);
+                    JOptionPane.showMessageDialog(null, "Delete successfully ^^");
+
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+    }
 }
